99c81af9571c2667454169e4a1138c8bc289cf54
Piyush Sharma <piyush316692@gmail.com>
2023-04-23 19:05:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
+import uuid
+
+app = Flask(__name__)
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, "Bad request. Ensure 'name' is included in the JSON payload.")
+    for store in stores:
+        if store_data["name"] == store["name"]:
+            abort(400, "Store already exists.")
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400, "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+    for item in item.values():
+        if(
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, f"{item} already exists.")
+    if item_data["store_id"] not in stores:
+        abort(404, message = "Store not found.")
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_id, "id": item_id}
+    items[item_id] = item
+
+    return item ,201     
+    
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message = "Store not found.")
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found."}, 404
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+   
+
+    
