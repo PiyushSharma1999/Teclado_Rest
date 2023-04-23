58fd33a527e95d92af8b754c6eaa4f297acba740
Piyush Sharma <piyush316692@gmail.com>
2023-04-23 20:38:24 +0530

    segregated stores and items methods

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ import uuid
 
 app = Flask(__name__)
 
+##################################################################################################################################################################################
+##########################################################******STORES METHODS******##############################################################################################
+
 @app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message = "Store not found.")
 
 @app.post("/store")
 def create_store():
@@ -21,6 +31,30 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message":"Store Deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+##################################################################################################################################################################################
+##########################################################******ITEMS METHODS******##############################################################################################
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found."}, 404
+
 
 @app.post("/item")
 def create_item():
@@ -46,24 +80,6 @@ def create_item():
 
     return item ,201     
     
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message = "Store not found.")
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        return {"message": "Item not found."}, 404
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
 @app.delete("/item/<string:item_id>")
 def delete_item(item_id):
     try:
@@ -71,14 +87,6 @@ def delete_item(item_id):
         return {"message":"Item Deleted."}
     except KeyError:
         abort(404, message="Item not found.")
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message":"Store Deleted."}
-    except KeyError:
-        abort(404, message="Store not found.")
 
 @app.put("/item/<string:item_id>")
 def update_item(item_id):
