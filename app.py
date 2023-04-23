ec1f5cb0641b0fa44a822fab97bde52dfd9ab768
Piyush Sharma <piyush316692@gmail.com>
2023-04-23 20:32:38 +0530

    added put and delete methods for  stores&items

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_store():
     store_data = request.get_json()
     if "name" not in store_data:
         abort(400, "Bad request. Ensure 'name' is included in the JSON payload.")
-    for store in stores:
+    for store in stores.values():
         if store_data["name"] == store["name"]:
             abort(400, "Store already exists.")
     store_id = uuid.uuid4().hex
@@ -31,17 +31,17 @@ def create_item():
         or "name" not in item_data
     ):
         abort(400, "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
-    for item in item.values():
+    for item in items.values():
         if(
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
         ):
             abort(400, f"{item} already exists.")
-    if item_data["store_id"] not in stores:
-        abort(404, message = "Store not found.")
+        if item_data["store_id"] not in stores:
+            abort(404, message = "Store not found.")
     
     item_id = uuid.uuid4().hex
-    item = {**item_id, "id": item_id}
+    item = {**item_data, "id": item_id}
     items[item_id] = item
 
     return item ,201     
@@ -64,6 +64,32 @@ def get_item(item_id):
 def get_all_items():
     return {"items": list(items.values())}
 
-   
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message":"Item Deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message":"Store Deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message = "Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
     
